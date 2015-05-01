70cbde006bd0b7e054874cda2f8215c67c4b7344
jimmy-jia <jimmy.j.jia@gmail.com>
2015-05-01 05:06:57 -0500

    new update

--- newjs.js
+++ newjs.js
@@ -2,44 +2,48 @@ var ref = new Firebase('https://hacker-news.firebaseio.com/v0/');
 var itemref = ref.child('item');
 var topstories = ref.child('topstories');
 var item = "https://hacker-news.firebaseio.com/v0/item/";
-var json = ".json";
-
+var newstories = ref.child('newstories');
+var orig = "https://news.ycombinator.com/item?id=";
 
 
 var i=0;
 var j=20;
-
+var postnum=1;
 function loadpage(){
 	topstories.on('value', function(postSnapshot){
 		var data=postSnapshot.val();
-		console.log(data);
 		while(data[i] && i<j){
 			var postnumber=data[i];
-			console.log(data[i]);
-			i++;
 			loaditem(postnumber);
+			i++;
 		}
 	});
 }
+function loadnew(){
+	newstories.on('value', function(postSnapshot){
+		var data=postSnapshot.val();
+		while(data[i] && i<j){
+			var postnumber=data[i];
+			loaditem(postnumber);
+			i++;
+		}
+	})
+}
 function loaditem(num){
-	console.log('loading');
 	var object = itemref.child(num);
 	object.once('value', function(dataSnapshot){
 		var obj=dataSnapshot.val();
-		console.log(obj);
 		var post = $('<div class="post"></div>');
+		$('<p/>').text(postnum).appendTo($(post));
 		$('<a/>').attr("href", obj.url).text(obj.title).appendTo($(post));
-		$('<p/>').text(obj.by).appendTo($(post));
-		$('<p/>').text(obj.score).appendTo($(post));
+		$('<p/>').text("+"+obj.score+" points").appendTo($(post));
+		$('<p/>').text("Posted by "+obj.by).appendTo($(post));
+		$('<a/>').attr("href", orig+obj.id).text(obj.descendants+" comments").appendTo($(post));
 		$("#postdiv").append(post);
+		postnum++;
 	})
-
 }
-function loadnew(){
-
-}
-function loadmore(){
+function loadMore(){
 	j+=20;
 	loadpage();
 }
-loadpage();
